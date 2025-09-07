f87c923e3ea38a9f2771fdbe5d5d5c3f78c84f4d
DESKTOP-QU25RS7\dev <whyis2cool@gmail.com>
2025-09-06 22:57:57 -0600

    Add type defs to use for the entire project

---
+++ src/helper_types.hpp
@@ -0,0 +1,15 @@
+#include <cstdint>
+
+#if !HELPER_TYPES_HPP
+#define HELPER_TYPES_HPP
+
+using i64 = int64_t;
+using i32 = int32_t;
+using i16 = int16_t;
+using i8 = int8_t;
+using u64 = uint64_t;
+using u32 = uint32_t;
+using u16 = uint16_t;
+using u8 = uint8_t;
+
+#endif
