03ddd117eadb949ccb5f53cb9575c8ec97e39576
DESKTOP-QU25RS7\dev <whyis2cool@gmail.com>
2025-09-07 00:03:11 -0600

    Finished (for now) a clase to represent a series of points and colors

--- src/color_points.cpp
+++ src/color_points.cpp
@@ -1,9 +1,11 @@
-
 #include "color_points.hpp"
 #include "godot_cpp/core/class_db.hpp"
+#include "godot_cpp/core/error_macros.hpp"
+#include "godot_cpp/variant/color.hpp"
+#include "godot_cpp/variant/vector2i.hpp"
 #include "helpers_for_godot.hpp"
-#include <cassert>
-#include <cstdint>
+
+namespace g = godot;
 
 void
 ColorPoints::add_color_point (const godot::Vector2i point,
@@ -14,10 +16,53 @@ ColorPoints::add_color_point (const godot::Vector2i point,
 }
 
 int64_t
-ColorPoints::get_count()const
+ColorPoints::get_count () const
 {
-    assert(colors.size() == points.size());
-    return colors.size();
+  return colors.size ();
+}
+
+godot::Color
+ColorPoints::get_color (const int64_t index) const
+{
+  const i64 size = colors.size ();
+  CRASH_BAD_INDEX_MSG (index, size, "[ERR] OUT OF INDEX ");
+
+  return colors[index];
+}
+
+g::Vector2i
+ColorPoints::get_point (const int64_t index) const
+{
+  const i64 size = colors.size ();
+  CRASH_BAD_INDEX_MSG (index, size, "[ERR] OUT OF INDEX ");
+  return points[index];
+}
+
+void
+ColorPoints::remove_at (const i64 index)
+{
+  const i64 color_size = colors.size ();
+  const i64 point_size = points.size ();
+  DEV_ASSERT (color_size == point_size);
+
+  colors.remove_at (index);
+  points.remove_at (index);
+}
+
+void
+ColorPoints::set_color(const i64 index,const g::Color& new_color)
+{
+  const i64 size = colors.size();
+  CRASH_BAD_INDEX_MSG (index, size, "[ERR] OUT OF INDEX ");
+  colors.set(index,new_color);
+}
+
+void
+ColorPoints::set_point(const i64 index,const g::Vector2i new_point)
+{
+  const i64 size = points.size();
+  CRASH_BAD_INDEX_MSG (index, size, "[ERR] OUT OF INDEX ");
+  points.set(index,new_point);
 }
 
 void
@@ -27,5 +72,17 @@ ColorPoints::_bind_methods ()
   using godot::D_METHOD;
   ClassDB::bind_method (D_METHOD ("add_color_point", "point", "color"),
                         &ColorPoints::add_color_point);
-  BIND_METHOD_NO_ARGS(ColorPoints, get_count);
+  BIND_METHOD_NO_ARGS (ColorPoints, get_count);
+
+  ClassDB::bind_method (D_METHOD ("get_color", "index"),
+                        &ColorPoints::get_color);
+
+  ClassDB::bind_method (D_METHOD ("get_point", "index"),
+                        &ColorPoints::get_point);
+
+  ClassDB::bind_method (D_METHOD ("set_color", "index"),
+                        &ColorPoints::set_color);
+
+  ClassDB::bind_method (D_METHOD ("set_point", "index"),
+                        &ColorPoints::set_point);
 }
--- src/color_points.hpp
+++ src/color_points.hpp
@@ -1,10 +1,10 @@
-
+#pragma once
 #include "godot_cpp/classes/object.hpp"
 #include "godot_cpp/classes/wrapped.hpp"
 #include "godot_cpp/templates/vector.hpp"
 #include "godot_cpp/variant/color.hpp"
 #include "godot_cpp/variant/vector2i.hpp"
-#include <cstdint>
+#include "helper_types.hpp"
 
 
 /**
@@ -19,18 +19,15 @@ public:
   //! Adds a new point and color to the class
   void add_color_point (const godot::Vector2i point, const godot::Color color);
   //! \returns how many color points does the object have.
-  int64_t get_count()const;
+  i64 get_count()const;
 
-  godot::Color get_color (const int64_t index) const;
-  godot::Vector2i get_point (const int64_t index) const;
+  godot::Color get_color (const i64 index) const;
+  godot::Vector2i get_point (const i64 index) const;
 
-  godot::Color& get_color_ref (const int64_t index);
-  godot::Vector2i& get_point_ref (const int64_t index);
+  void remove_at (const i64 index) ;
 
-  void remove_at (const int64_t index) const;
-
-  void set_color (const int64_t index) const;
-  void set_point (const int64_t index) const;
+  void set_color (const i64 index, const godot::Color& new_color) ;
+  void set_point (const i64 index, const godot::Vector2i new_point) ;
 
 protected:
   static void _bind_methods ();
