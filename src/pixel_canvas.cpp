eec76b92983b4eb8f9a3379cd61c8598cc2b9ced
DESKTOP-QU25RS7\dev <whyis2cool@gmail.com>
2025-09-07 00:03:53 -0600

    PixelCanvas can now use ColorPoints

--- src/pixel_canvas.cpp
+++ src/pixel_canvas.cpp
@@ -1,16 +1,19 @@
 #include "pixel_canvas.hpp"
-#include "godot_cpp/classes/canvas_item.hpp"
+#include "color_points.hpp"
 #include "godot_cpp/classes/image.hpp"
 #include "godot_cpp/classes/image_texture.hpp"
 #include "godot_cpp/classes/packed_data_container.hpp"
 #include "godot_cpp/classes/ref.hpp"
 #include "godot_cpp/core/class_db.hpp"
+#include "godot_cpp/core/error_macros.hpp"
 #include "godot_cpp/core/object.hpp"
 #include "godot_cpp/core/print_string.hpp"
 #include "godot_cpp/core/property_info.hpp"
 #include "godot_cpp/variant/array.hpp"
 #include "godot_cpp/variant/color.hpp"
 #include "godot_cpp/variant/packed_byte_array.hpp"
+#include "godot_cpp/variant/packed_color_array.hpp"
+#include "godot_cpp/variant/packed_vector2_array.hpp"
 #include "godot_cpp/variant/string.hpp"
 #include "godot_cpp/variant/variant.hpp"
 #include "helpers_for_godot.hpp"
@@ -22,14 +25,14 @@ namespace gd = godot;
 static constexpr int64_t red[4] = { 0xbb, 0x00, 0x00, 0xff };
 static constexpr int64_t blue[4] = { 0x00, 0xbb, 0x00, 0xff };
 static constexpr int64_t green[4] = { 0x00, 0x00, 0xbb, 0xff };
-static constexpr int64_t half_transparent[4] = { 0x00, 0x00, 0x00, 0xff / 4 };
+static constexpr int64_t half_transparent[4] = { 0x00, 0x00, 0x00, 0xff / 2 };
 
 bool
 PixelCanvas::start ()
 {
   ref_image = create_image_default ();
   ref_texture = godot::ImageTexture::create_from_image (ref_image);
-  // this->create_from_image(ref_image);
+  data_array.resize(DEFAULT_HEIGHT * DEFAULT_WIDTH * get_format_color_offset());
   ref_texture->set_image (ref_image);
   has_started = true;
   return has_started;
@@ -48,16 +51,39 @@ PixelCanvas::test_is_working () const
 }
 
 void
-PixelCanvas::draw_points(const godot::PackedVector2Array& points_to_draw)
+PixelCanvas::draw_points (const godot::PackedVector2Array &points_to_draw)
 {
-  if(!check_if_pixel_canvas_was_started()) {return;}
+  if (!check_if_pixel_canvas_was_started ())
+    {
+      return;
+    }
+
+  godot::PackedByteArray data = ref_image->get_data ();
+  const int64_t format_color_offset = get_format_color_offset ();
+  const int64_t row_offset = ref_image->get_width ();
+
+  for (const gd::Vector2 &vec : points_to_draw)
+    {
+      const int64_t byte_index
+          = (row_offset * static_cast<int64_t> (vec.y) * format_color_offset)
+            + (static_cast<int64_t> (vec.x) * format_color_offset);
+      data.set (byte_index + 0, current_color.get_r8 ());
+      data.set (byte_index + 1, current_color.get_g8 ());
+      data.set (byte_index + 2, current_color.get_b8 ());
+      data.set (byte_index + 3, current_color.get_a8 ());
+    }
+
+  update_texture (data);
 }
 
 void
 PixelCanvas::draw_line_l (const godot::Vector2i start,
                           const godot::Vector2i end)
 {
-  if(!check_if_pixel_canvas_was_started()) {return;}
+  if (!check_if_pixel_canvas_was_started ())
+    {
+      return;
+    }
   gd::print_line (gd::String ("[TODO]: Implement draw line"));
 }
 
@@ -65,7 +91,10 @@ void
 PixelCanvas::draw_rainbow_effect ()
 {
 
-  if(!check_if_pixel_canvas_was_started()) {return;}
+  if (!check_if_pixel_canvas_was_started ())
+    {
+      return;
+    }
   const float inverse_width = 1.0f / ref_image->get_width ();
   const float inverse_height = 1.0f / ref_image->get_height ();
   const float inverse_size
@@ -87,18 +116,21 @@ void
 PixelCanvas::draw_on_data ()
 {
 
-  if(!check_if_pixel_canvas_was_started()) {return;}
+  if (!check_if_pixel_canvas_was_started ())
+    {
+      return;
+    }
   gd::print_line ("start 'draw_on_data'");
   gd::PackedByteArray data = ref_image->get_data ();
-  
 
   const int64_t row_offset = ref_image->get_width ();
   int64_t colum_offset = 0;
-  const int64_t format_color_offset = get_format_color_offset();
+  const int64_t format_color_offset = get_format_color_offset ();
 
   for (int64_t row = 0; row < ref_image->get_height (); ++row)
     {
-      int64_t index = (row_offset * row * format_color_offset) + (colum_offset * format_color_offset);
+      int64_t index = (row_offset * row * format_color_offset)
+                      + (colum_offset * format_color_offset);
       data.set (index + 0, red[0]);
       data.set (index + 1, red[1]);
       data.set (index + 2, red[2]);
@@ -107,7 +139,7 @@ PixelCanvas::draw_on_data ()
     }
   gd::print_line (gd::String ("size of data = %s") % data.size ());
 
-  update_texture(data);
+  update_texture (data);
 
   gd::print_line ("end 'draw_on_data'");
 }
@@ -115,7 +147,10 @@ PixelCanvas::draw_on_data ()
 void
 PixelCanvas::draw_diagnal_rgb_effect ()
 {
-  if(!check_if_pixel_canvas_was_started()) {return;}
+  if (!check_if_pixel_canvas_was_started ())
+    {
+      return;
+    }
 
   gd::PackedByteArray data = ref_image->get_data ();
 
@@ -150,14 +185,17 @@ PixelCanvas::draw_diagnal_rgb_effect ()
         }
     }
 
-  update_texture(data);
+  update_texture (data);
 }
 
 void
 PixelCanvas::draw_pure_white_canvas ()
 {
 
-  if(!check_if_pixel_canvas_was_started()) {return;}
+  if (!check_if_pixel_canvas_was_started ())
+    {
+      return;
+    }
   gd::PackedByteArray data = ref_image->get_data ();
   gd::print_line (gd::String ("size of data = %s") % data.size ());
 
@@ -166,7 +204,46 @@ PixelCanvas::draw_pure_white_canvas ()
       data.set (byte, 0xff);
     }
 
-  update_texture(data);
+  update_texture (data);
+}
+
+void
+PixelCanvas::draw_color_points (const ColorPoints *color_points)
+{
+  if (!check_if_pixel_canvas_was_started ())
+    {
+      return;
+    }
+
+  if (color_points == nullptr)
+    {
+      ERR_PRINT ("[ERR]:passed null pointer as argument (don't do that)");
+      return;
+    }
+
+
+  const i64 colors_len = color_points->colors.size ();
+  const i64 points_len = color_points->points.size ();
+  DEV_ASSERT (colors_len == points_len);
+
+  const i64 format_color_offset = get_format_color_offset ();
+  const i64 offset_per_row = format_color_offset * ref_image->get_width ();
+  const i64 colum_offset = format_color_offset;
+
+  gd::PackedByteArray data = ref_image->get_data ();
+
+  for (i64 i = 0; i < points_len; ++i)
+    {
+      const i64 base_index = (color_points->points[i].y * offset_per_row)
+                             + (color_points->points[i].x * colum_offset);
+      const gd::Color &color = color_points->colors[i];
+      data.set (base_index, color.get_r8 ());
+      data.set (base_index + 1, color.get_g8 ());
+      data.set (base_index + 2, color.get_b8 ());
+      data.set (base_index + 3, color.get_a8 ());
+    }
+  
+    update_texture(data);
 }
 
 godot::Ref<godot::Image>
@@ -246,13 +323,13 @@ PixelCanvas::set_color (const gd::Color new_color)
 }
 
 int64_t
-PixelCanvas::get_format_color_offset()const 
+PixelCanvas::get_format_color_offset () const
 {
 
   int64_t color_component_offset = 0;
-  const gd::Image::Format current_format = ref_image->get_format();
-  switch (current_format) 
-  {
+  const gd::Image::Format current_format = ref_image->get_format ();
+  switch (current_format)
+    {
     case gd::Image::Format::FORMAT_RGB8:
       color_component_offset = 3;
       break;
@@ -260,24 +337,26 @@ PixelCanvas::get_format_color_offset()const
     case gd::Image::Format::FORMAT_RGBA8:
       color_component_offset = 4;
       break;
-  
-      default:
-      gd::String error_string = gd::String("") ;// only support
-      error_string.insert(0,"ONLY SUPPORT THE FORMATS ");
-      //gd::Image::Format::FORMAT_RGBA8
-      error_string.insert(error_string.length(),"FORMAT_RGB8 and FORMAT_RGBA8 ");
-      error_string.insert(error_string.length(),", WE DON'T SUPPORT THE FORMAT AT INDEX [%s] ");
-      error_string % static_cast<int64_t>(current_format);
-      gd::print_error(error_string);
-    
+
+    default:
+      gd::String error_string = gd::String (""); // only support
+      error_string.insert (0, "ONLY SUPPORT THE FORMATS ");
+      // gd::Image::Format::FORMAT_RGBA8
+      error_string.insert (error_string.length (),
+                           "FORMAT_RGB8 and FORMAT_RGBA8 ");
+      error_string.insert (error_string.length (),
+                           ", WE DON'T SUPPORT THE FORMAT AT INDEX [%s] ");
+      error_string % static_cast<int64_t> (current_format);
+      gd::print_error (error_string);
+
       break;
-  }
+    }
 
   return color_component_offset;
 }
 
 void
-PixelCanvas::update_texture(const godot::PackedByteArray& texture_update)
+PixelCanvas::update_texture (const godot::PackedByteArray &texture_update)
 {
   auto format = ref_image->get_format ();
   ref_image->set_data (width, height, false, format, texture_update);
@@ -285,13 +364,14 @@ PixelCanvas::update_texture(const godot::PackedByteArray& texture_update)
 }
 
 bool
-PixelCanvas::check_if_pixel_canvas_was_started()const
+PixelCanvas::check_if_pixel_canvas_was_started () const
 {
-  if(!has_started)
-  {
-    gd::print_error("you need to call the 'start' function for this to work");
-    return has_started;
-  }
+  if (!has_started)
+    {
+      gd::print_error (
+          "you need to call the 'start' function for this to work");
+      return has_started;
+    }
 
   return has_started;
 }
@@ -314,47 +394,50 @@ PixelCanvas::_bind_methods ()
   BIND_METHOD_NO_ARGS (PixelCanvas, draw_on_data);
   BIND_METHOD_NO_ARGS (PixelCanvas, draw_diagnal_rgb_effect);
   BIND_METHOD_NO_ARGS (PixelCanvas, draw_pure_white_canvas);
-  BIND_METHOD_NO_ARGS (PixelCanvas, draw_points);
+  //  BIND_METHOD_NO_ARGS (PixelCanvas, draw_points);
 
-  gd::ClassDB::bind_method (D_METHOD ("draw_line_l", "start", "end"),
-                            &PixelCanvas::draw_line_l);
+  ClassDB::bind_method (D_METHOD ("draw_points", "Points"),
+                        &PixelCanvas::draw_points);
+  ClassDB::bind_method (D_METHOD ("draw_line_l", "start", "end"),
+                        &PixelCanvas::draw_line_l);
 
-  gd::ClassDB::bind_method (D_METHOD ("get_image"), &PixelCanvas::get_image);
+  ClassDB::bind_method (D_METHOD ("draw_color_points", "color_points"),
+                        &PixelCanvas::draw_color_points);
 
-  gd::ClassDB::bind_method (D_METHOD ("get_texture"),
-                            &PixelCanvas::get_texture);
+  ClassDB::bind_method (D_METHOD ("get_image"), &PixelCanvas::get_image);
 
-  gd::ClassDB::bind_static_method (get_class_static (),
-                                   D_METHOD ("DEFAULT_COLOR"),
-                                   &PixelCanvas::DEFAULT_COLOR);
+  ClassDB::bind_method (D_METHOD ("get_texture"), &PixelCanvas::get_texture);
 
-  gd::ClassDB::bind_static_method (get_class_static (),
-                                   D_METHOD ("create_image_default"),
-                                   &PixelCanvas::create_image_default);
+  ClassDB::bind_static_method (get_class_static (), D_METHOD ("DEFAULT_COLOR"),
+                               &PixelCanvas::DEFAULT_COLOR);
+
+  ClassDB::bind_static_method (get_class_static (),
+                               D_METHOD ("create_image_default"),
+                               &PixelCanvas::create_image_default);
 
   /// PROPERTIES
-  gd::ClassDB::bind_method (D_METHOD ("set_width", "new_width"),
-                            &PixelCanvas::set_width);
-  gd::ClassDB::bind_method (D_METHOD ("get_width"), &PixelCanvas::get_width);
+  ClassDB::bind_method (D_METHOD ("set_width", "new_width"),
+                        &PixelCanvas::set_width);
+  ClassDB::bind_method (D_METHOD ("get_width"), &PixelCanvas::get_width);
   ADD_PROPERTY (PropertyInfo (Variant::INT, "width"), "set_width",
                 "get_width");
 
-  gd::ClassDB::bind_method (D_METHOD ("set_height", "new_height"),
-                            &PixelCanvas::set_height);
-  gd::ClassDB::bind_method (D_METHOD ("get_height"), &PixelCanvas::get_height);
+  ClassDB::bind_method (D_METHOD ("set_height", "new_height"),
+                        &PixelCanvas::set_height);
+  ClassDB::bind_method (D_METHOD ("get_height"), &PixelCanvas::get_height);
   ADD_PROPERTY (PropertyInfo (Variant::INT, "height"), "set_height",
                 "get_height");
 
   /// CONSTANTS
   ///
-  gd::ClassDB::bind_integer_constant (
-      get_class_static (), "", "DEFAULT_HEIGHT", PixelCanvas::DEFAULT_HEIGHT);
+  ClassDB::bind_integer_constant (get_class_static (), "", "DEFAULT_HEIGHT",
+                                  PixelCanvas::DEFAULT_HEIGHT);
 
-  gd::ClassDB::bind_integer_constant (get_class_static (), "", "DEFAULT_WIDTH",
-                                      PixelCanvas::DEFAULT_WIDTH);
+  ClassDB::bind_integer_constant (get_class_static (), "", "DEFAULT_WIDTH",
+                                  PixelCanvas::DEFAULT_WIDTH);
 
-  gd::ClassDB::bind_method (D_METHOD ("set_color"), &PixelCanvas::set_color);
-  gd::ClassDB::bind_method (D_METHOD ("get_color"), &PixelCanvas::get_color);
+  ClassDB::bind_method (D_METHOD ("set_color"), &PixelCanvas::set_color);
+  ClassDB::bind_method (D_METHOD ("get_color"), &PixelCanvas::get_color);
 
   ADD_PROPERTY (PropertyInfo (Variant::COLOR, "current_color"), "set_color",
                 "get_color");
--- src/pixel_canvas.hpp
+++ src/pixel_canvas.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "godot_cpp/classes/node2d.hpp"
 #include "godot_cpp/classes/ref.hpp"
 #include "godot_cpp/variant/color.hpp"
 #include "godot_cpp/variant/packed_byte_array.hpp"
@@ -9,13 +8,14 @@
 #include <godot_cpp/classes/image.hpp>
 #include <godot_cpp/classes/image_texture.hpp>
 #include <godot_cpp/classes/resource.hpp>
-//#include "godot_cpp/templates/vector.hpp"
 
 #include <cstdint>
 
-class PixelCanvas : public godot::Node2D
+class ColorPoints;
+
+class PixelCanvas : public godot::Resource
 {
-  GDCLASS (PixelCanvas, godot::Node2D)
+  GDCLASS (PixelCanvas, godot::Resource)
 
 public:
   static constexpr int32_t DEFAULT_WIDTH = 64;
@@ -28,12 +28,13 @@ public:
   bool test_is_working () const;
 
   // Draw
-  void draw_points(const godot::PackedVector2Array& points_to_draw);
+  void draw_points (const godot::PackedVector2Array &points_to_draw);
   void draw_line_l (const godot::Vector2i start, const godot::Vector2i end);
   void draw_rainbow_effect ();
   void draw_on_data ();
-  void draw_diagnal_rgb_effect();
-  void draw_pure_white_canvas();
+  void draw_diagnal_rgb_effect ();
+  void draw_pure_white_canvas ();
+  void draw_color_points(const ColorPoints*ptr_color_points);
 
   /// GETTERS
   godot::Ref<godot::Image> get_image () const;
@@ -48,11 +49,11 @@ public:
   void set_width (int32_t new_width);
   void set_height (int32_t new_height);
   void set_color (const godot::Color new_color);
-  
+
 private:
-  int64_t get_format_color_offset()const;
-  void update_texture(const godot::PackedByteArray& texture_update);
-  bool check_if_pixel_canvas_was_started()const;
+  int64_t get_format_color_offset () const;
+  void update_texture (const godot::PackedByteArray &texture_update);
+  bool check_if_pixel_canvas_was_started () const;
 
 public:
   static godot::Ref<godot::Image> create_image_default ();
@@ -68,6 +69,8 @@ private:
       = godot::Ref<godot::ImageTexture> ();
 
   godot::Color current_color = godot::Color ("#c01111c0");
+  
+  godot::PackedByteArray data_array = godot::PackedByteArray();
   int32_t width = DEFAULT_WIDTH;
   int32_t height = DEFAULT_HEIGHT;
   bool has_started = false;
